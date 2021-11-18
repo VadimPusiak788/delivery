b21b5be84afdf59704ae7b81de97e227f223bc86
VadimPusiak788 <vpusak629@gmail.com>
2021-11-18 18:07:03 +0200

    first commit

---
+++ frontend/src/components/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { render } from "react-dom";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false,
+      placeholder: "Loading"
+    };
+  }
+
+
+  render() {
+    return (
+      <ul>
+          return (
+            <li>
+              fdsdf
+            </li>
+          );
+      </ul>
+    );
+  }
+}
+
+export default App;
+
+const container = document.getElementById("app");
+render(<App />, container);
