31b52c43a234b1b1cd1ab1f31f6928013e1c15a2
VadimPusiak788 <vpusak629@gmail.com>
2021-12-15 21:53:42 +0200

    git

--- delivery/settings.py
+++ delivery/settings.py
@@ -140,8 +140,8 @@ AUTH_USER_MODEL = 'user.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
