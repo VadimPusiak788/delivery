aca57fd33eafa3463c4c682ed83e8208cb6556e6
VadimPusiak788 <vpusak629@gmail.com>
2021-12-15 22:49:44 +0200

    stat

--- delivery/settings.py
+++ delivery/settings.py
@@ -141,11 +141,11 @@ AUTH_USER_MODEL = 'user.User'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/static'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
