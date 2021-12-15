812677b09036210dc0f61106df5c7ac6d67ee54f
VadimPusiak788 <vpusak629@gmail.com>
2021-12-15 22:27:21 +0200

    static

--- delivery/settings.py
+++ delivery/settings.py
@@ -140,16 +140,13 @@ AUTH_USER_MODEL = 'user.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATICFILES_DIRS = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 
-WHITENISE_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'root') 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
