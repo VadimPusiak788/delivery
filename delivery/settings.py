ba4b7af12426b1d44f9efb0e957e7e9a73daaaed
VadimPusiak788 <vpusak629@gmail.com>
2021-12-15 21:41:56 +0200

    add names

--- delivery/settings.py
+++ delivery/settings.py
@@ -138,9 +138,13 @@ AUTH_USER_MODEL = 'user.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
 WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'root') 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
