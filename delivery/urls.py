f8390af97f9de4badd43bd8e9238df7480965ef7
VadimPusiak788 <vpusak629@gmail.com>
2021-12-15 22:23:03 +0200

    static

--- delivery/urls.py
+++ delivery/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -49,4 +51,5 @@ urlpatterns = [
     path('', include('frontend.urls')),
     path('api/order/', include('order.urls'))
     
-] 
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
