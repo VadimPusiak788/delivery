749d397d7e1e9726704f6f3e5ba9df592c3e7b16
VadimPusiak788 <vpusak629@gmail.com>
2021-12-15 22:51:49 +0200

    statasd

--- delivery/settings.py
+++ delivery/settings.py
@@ -141,7 +141,7 @@ AUTH_USER_MODEL = 'user.User'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static'
+STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
